c7c7f49a6ae6282444d93552ddc27efb94333eb7
zhangmyihua <michaelzhang2@msn.com>
2015-01-22 15:44:09 -0500

    finished the first movement of rover

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,15 +1,10 @@
-puts "What is the size of the plateau?"
-area = gets.chomp
-puts "Where is the first rover?"
-position1 = gets.chomp
-puts "What do you want the first rover to do?"
-directions1 = gets.chomp
 
-position1_array = position1.split(" ")
+#position1_array = []
 
+#read_instructions1_array = [L,R,M,M,L,M]
 
 class Rover
-	attr_accessor :x, :y, :direction
+	attr_accessor :x, :y, :d
 
 	def initialize(x, y, d)
 		@x = x
@@ -17,9 +12,60 @@ class Rover
 		@d = d
 	end 
 
-	def read_intruction
+	def read_instructions(instructions)
+		instructions.each do |a|
+			if a == "M"
+				then move
+			elsif a == "L" || a == "R"
+				then turn(a)
+			end
+		end
 	end
+
+	def turn(turn_dir)
+		if @d == "N" && turn_dir == "L" then @d = "W"
+			elsif @d == "N" && turn_dir == "R" then @d = "E"
+			elsif @d == "W" && turn_dir == "L" then @d = "S"
+			elsif @d == "W" && turn_dir == "R" then @d = "N"
+			elsif @d == "E" && turn_dir == "L" then @d = "N"
+			elsif @d == "E" && turn_dir == "R" then @d = "S"
+			elsif @d == "S" && turn_dir == "L" then @d = "E"
+			elsif @d == "S" && turn_dir == "R" then @d = "W"
+		end
+	end
+ 			
+	def move
+		if @d == "N" then @y += 1
+		elsif @d == "S" then @y -= 1
+		elsif @d == "E" then @x += 1
+		elsif @d == "W" then @x -= 1
+	end
+	end
+
+	def to_s
+		puts "#{@x} #{@y} #{@d}"
+	end
+
 end
+
+
+puts "What is the size of the plateau?"
+area = gets.chomp
+puts "Where is the first rover?"
+position1 = gets.chomp
+puts "What do you want the first rover to do?"
+read_instructions1 = gets.chomp
+
+position1_array = position1.split(" ")
+
+read_instructions1_array = read_instructions1.split("")
+
+
+rover1 = Rover.new(position1_array[0].to_i, position1_array[1].to_i, position1_array[2])
+
+rover1.read_instructions(read_instructions1_array)
+rover1.to_s
+
 
 
 #first input, what is the size of the plateau (x, y)
